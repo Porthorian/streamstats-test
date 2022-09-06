425b5b44d854d9d40706d1ea3ded530caf523dae
Vinnie Marone <vinnie.marone35@gmail.com>
2022-09-06 08:52:24 -0400

    More cors enhancements

--- api/src/Util/ResponseHelper.php
+++ api/src/Util/ResponseHelper.php
@@ -39,7 +39,8 @@ class ResponseHelper
 	{
 		return $response
 			->withHeader('Access-Control-Allow-Origin', 'http://localhost:8080')
+			->withHeader('Access-Control-Allow-Credentials', 'true')
 			->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-			->withHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+			->withHeader('Access-Control-Allow-Methods', 'GET');
 	}
 }
