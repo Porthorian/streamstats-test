75337d5334f5464164d589c308b395c2eb955bab
Vinnie Marone <vinnie.marone35@gmail.com>
2022-09-06 08:49:09 -0400

    Restrict to only options and GET

--- api/src/Util/ResponseHelper.php
+++ api/src/Util/ResponseHelper.php
@@ -40,6 +40,6 @@ class ResponseHelper
 		return $response
 			->withHeader('Access-Control-Allow-Origin', 'http://localhost:8080')
 			->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-			->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+			->withHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
 	}
 }
